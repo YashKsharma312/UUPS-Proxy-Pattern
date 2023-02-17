c0de5dec0e671ac8aac95661f34a1ed3e95b3a74
YashKsharma312 <112351428+YashKsharma312@users.noreply.github.com>
2023-02-17 16:21:51 +0530

    init commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,12 +7,7 @@ async function main(){
     await logic.deployed();
     console.log("Logic Proxy Contract deployed to : ",logic.address);
     console.log("Logic Contract implementation address is : ",await upgrades.erc1967.getImplementationAddress(logic.address));
-    console.log("Deploying LogicV2 contract...");
-    const LogicV2 = await ethers.getContractFactory("LogicV2");
-    const logicv2 = await upgrades.upgradeProxy(logic.address,LogicV2);
-    await logicv2.deployed();
-    console.log("LogicV2 Proxy Contract ( Must be Same ) deployed to : ",logicv2.address);
-    console.log("LogicV2 Contract implementation address is : ",await upgrades.erc1967.getImplementationAddress(logicv2.address));
+    
 }
 
 main();
